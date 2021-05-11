13a64de4930297d4ee56e573c5f0db35de7f2145
JosielMatos <josielmatos77@protonmail.com>
2021-05-11 11:18:27 -0300

    google auth initial set

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 
+//config
 dotenv.config({path: './config/config.env'})
+require('./config/passport')(passport)
 connectDB()
 
 //dev env requests logging
@@ -16,6 +20,16 @@ if (process.env.NODE_ENV === 'development') {
 //Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 //static folder
 app.use(express.static('public'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('login', {
-        layout: 'login',
-    })
-})
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 router.get('/dashboard', (req, res) => {
     res.render('dashboard')
